e9458556519d176e5a6c02781cc11d5d91c15fe1
aspnetxm <aspnetxm@xmisp.com>
2016-10-18 18:38:55 +0800

    Panda.Framework

---
+++ Project/Panda.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Panda.Web.Startup))]
+namespace Panda.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
